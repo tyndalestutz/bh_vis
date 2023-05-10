f131451a80721cb4e8aa9622de2a8aeb5a920749
tyndalestutz <tyndalestutz@gmail.com>
2023-05-10 15:23:51 -0700

    changed the following files

--- scripts/gwMeshGenerator.py
+++ scripts/gwMeshGenerator.py
@@ -3,8 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import sph_harm
 
+'''
+To Do:
+- Compile all l, m, strain values given
 
-#file parameters
+'''
+
+
+# File parameters
 folder_name = "gw_test"
 input_file = "/home/guest/Documents/BH_Vis/data/gw_data/Rpsi4_l2-r0100.0_strain.txt"
 parent_directory = os.path.dirname(os.path.dirname(__file__))
@@ -19,53 +25,26 @@ m = 2
 num_points_x = 200
 num_points_y = 200
 num_points_z = 1
-R_ext = 100 # radius of data extraction we are taking the gw strain from
+R_ext = 100 # Radius of data extraction we are taking the gw strain from
 
-# Most likely this will not be used
-propogation_speed = 1 #how many pixels the gw travels in one delta t, i.e. (c/ units of time per second) * (pixels/meter)
-
-#note: maybe make amplitude part of the render script instead of this script?
-amplitude = 100  # Set the amplitude of the wave visualization.
-
-#interpolated array allows for non-integer indecies and negative indecies return 0
-######## this is currently unused for most recent render
-class InterpolatedArray:
-    def __init__(self, data):
-        self.data = data
-        
-    def __getitem__(self, key):
-        data = self.data
-        if key > len(data) - 1:
-            print(f"IndexError: index {key} is out of bounds for array with size {len(data)}")
-            exit()
-        elif key < 0: 
-            return 0
-        elif key == int(key):
-            return data[key]
-        else:
-            slope = (data[int(key) + 1] - data[int(key)])
-            return (slope * (key - int(key))) + data[int(key)] #linear interpolation m(x1 - x0) + x0
-
-##WARNING: figure out spin-weoight factor
+# Calculate spin-weighted spherical harmonics for every point in the mesh, returns them as complex values in a 2d array
 def set_sph_harm_array(l,m,s):
     global num_points_x, num_points_y
 
+    # Store values in 2d array
     sph_harm_points = np.zeros((num_points_x,num_points_y),dtype=np.complex128) 
+
 
     for j in range(num_points_y):
         y = -(num_points_y - 1) + 2 * j
         for i in range(num_points_x):
             x = -(num_points_x - 1) + 2 * i
             r = np.sqrt(x**2 + y**2)
-            theta = np.pi/2
-            #theta = np.arccos(2/r) # Etienne said to use this for theta?
+            theta = np.pi/2 # For 2d purposes, theta (polar angle) will be constant
             phi = np.arctan2(y, x)
-            Y_lm = sph_harm(l, m, phi, theta)
-            # compute the spin-weighted spherical harmonic
-            Y = (-1)**s * np.sqrt((2*l+1)/(4*np.pi) * np.math.factorial(l-m)/np.math.factorial(l+m)) * Y_lm
-
+            Y_lm = sph_harm(l, m, phi, theta) # Calculate spherical harmonics using scipy.special funciton
+            Y = (-1)**s * np.sqrt((2*l+1)/(4*np.pi) * np.math.factorial(l-m)/np.math.factorial(l+m)) * Y_lm # incorporate spin-weight factor
             sph_harm_points[i,j] = Y
-    print(sph_harm_points)
     return sph_harm_points
 
 
@@ -81,6 +60,7 @@ def interpolated_strain(target_time, source_time, data):
     interpolated_data = np.interp(target_time, source_time, data)
     return interpolated_data
 
+# Reads inputted strain data - returns data file row length, initial strain data (complex), spin-weighted spherical harmonics (complex), and time values
 def initialize():
     # If output directory exists and has items, asks before the program overwrites. If the directory does not exist, the program exits
     if os.path.exists(output_directory):
@@ -113,29 +93,27 @@ def initialize():
     # Sort by time and remove duplicates
     strain_data = np.unique(strain_data, axis=0)
 
-    # separate time, real and imainary parts of h
+    # Separate time, real and imainary parts of h
     h_time, h_real, h_imag = strain_data[:, 0], strain_data[:, 1], strain_data[:, 2]
     h_strain = h_real + 1j*h_imag
     
 
     length = len(h_strain)
-    #h_strain = InterpolatedArray(h_strain) #allows non-integer indecies of h_strain
+
+    # Set spin-weighted spherical harmonics
     sph_harm_points = set_sph_harm_array(2,2,-2)
 
     return length, h_strain, sph_harm_points, h_time
-    
+
+# Assign values from initialize() function
 length, h_strain, sph_harm_points, h_time = initialize()
 
 start_time = time.time() #start timer
 percentage = np.round(np.linspace(0, length, 101)).astype(int) #creates an array of 10% points
 
-def total_strain_real(strain, R_ext, t, x, y):
-    r = np.sqrt(x**2 + y**2)
-    strain_real = sph_harm_points.real*strain[t].real - sph_harm_points.imag*strain[t].imag
-    return strain_real
 
-# loop through all time values in the data
-state = 0 # just for file naming purposes
+# Loop through all time values in the data
+state = 0 # For file naming purposes (***probably a better way to name them than this)
 for current_time in h_time:
     state += 1
     # Create mesh
@@ -157,10 +135,10 @@ for current_time in h_time:
         for i in range(num_points_x):
             x = -(num_points_x - 1) + 2 * i
             current_r = np.sqrt(x**2 + y**2)
-            # for every point in the mesh, calculate the adjusted time to find the strain at based on radius, simulation time, and extraction radius
+            # Ror every point in the mesh, calculate the adjusted time to find the strain at based on radius, simulation time, and extraction radius
             target_time = current_time - current_r + R_ext
             
-            # find initial and final times in the data, constrain target_time within those values
+            # Find initial and final times in the data, constrain target_time within those values
             time_0 = np.min(h_time)
             time_f = np.max(h_time)
             if (target_time < time_0):
@@ -168,25 +146,14 @@ for current_time in h_time:
             elif (target_time > time_f):
                 target_time = time_f
 
-            # find the intermediate strain that's dependent on current time and data extraction radius
+            # Find the intermediate strain that's dependent on current time and data extraction radius
             h_tR = interpolated_strain(target_time, h_time, h_strain)
-            # find the spin weighted spherical harmonic from the data array made for the current point
+            # Find the spin weighted spherical harmonic from the data array made for the current point
             Y = sph_harm_points[i, j]
 
             #Plot z based on the real part of the product of h_tR and Y
             z = Y.real*h_tR.real - Y.imag*h_tR.imag
             points.InsertNextPoint(x, y, z*100)
-            #print(x, y, z)
-            '''
-            for time in h_time:
-
-                z = total_strain_real(h_strain, R_ext, time, x, y) * amplitude
-                #h_total = sph_harm_points[i,j]*h_strain[t - int(r/propogation_speed)]
-
-                # plot the real part of the strain into z
-                #z = h_total.real * amplitude 
-                points.InsertNextPoint(x, y, z)
-            '''
             
     grid.SetPoints(points)
 
@@ -196,4 +163,5 @@ for current_time in h_time:
     writer.SetFileName(filename)
     writer.SetInputData(grid)
     writer.Write()
+
 print("Mesh database completed in",output_directory)
