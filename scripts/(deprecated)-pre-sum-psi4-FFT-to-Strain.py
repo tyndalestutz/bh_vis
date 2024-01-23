1e9eccd94489f3003a8bae7f2fd8df5e9f386f0a
Ledge-Dragmire <n.ledge@proton.me>
2024-01-23 14:33:07 -0800

    Core calculation issues

--- scripts/(deprecated)-pre-sum-psi4-FFT-to-Strain.py
+++ scripts/(deprecated)-pre-sum-psi4-FFT-to-Strain.py
@@ -1,8 +1,9 @@
 import os, scipy, sys, quaternionic, spherical
-import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-def read_modes_data(data_dir, ell_max):
+import numpy as np
 
+def read_modes_data(data_dir, ell_max):
     if not os.path.isdir(data_dir):
         print(f"Directory {data_dir} does not exist. Exiting...")
         exit()
@@ -21,7 +22,6 @@ def read_modes_data(data_dir, ell_max):
 
         data = np.array([list(map(np.float64, line.split())) for line in lines])
         #data2 = np.array([np.array(line.split(), dtype=np.float64) for line in lines])
-
         if len(unique_times) == 0:
             unique_times = np.unique(data[:, 0])
 
@@ -38,17 +38,7 @@ def read_modes_data(data_dir, ell_max):
 
     return unique_times, mode_data
 
-def compute_first_derivative(time, data):
-    """
-    Calculates the time derivative of the input data using a second-order finite difference stencil.
-
-    Args:
-        time (numpy.ndarray): A numpy array containing time values.
-        data (numpy.ndarray): A numpy array containing the data to be differentiated.
-
-    Returns:
-        numpy.ndarray: A numpy array containing the time derivative of the input data.
-    """
+def derivative_finite_difference(time, data): #only use on l=m=2
     dt = time[1] - time[0]
     derivative = np.zeros_like(data)
     # Second-order in the interior
@@ -56,21 +46,9 @@ def compute_first_derivative(time, data):
     # Drop to first-order at the endpoints
     derivative[0] = (data[1] - data[0]) / dt
     derivative[-1] = (data[-1] - data[-2]) / dt
-
     return derivative
 
-def process_wave_data(time, cmplx):
-    """
-    Calculates the cumulative phase and amplitude of a gravitational wave signal.
-
-    Args:
-        time (numpy.ndarray): A numpy array containing time values.
-        real (numpy.ndarray): A numpy array containing the real part of the signal.
-        imag (numpy.ndarray): A numpy array containing the imaginary part of the signal.
-
-    Returns:
-        tuple: A tuple containing three numpy arrays (amplitude, cumulative phase, phase time-derivative).
-    """
+def cumulative_phase_data(time, cmplx): #only use on l=m=2 
     phases = np.angle(cmplx)
 
     cycles = 0
@@ -81,15 +59,14 @@ def process_wave_data(time, cmplx):
             cycles -= 1
         if phase - last_phase < -np.pi:
             cycles += 1
-
         phases[index] += 2 * np.pi * cycles
         last_phase = phase
 
-    phases_dt = compute_first_derivative(time, phases)
+    phases_dt = derivative_finite_difference(time, phases)
 
     return  np.abs(cmplx), phases, phases_dt
 
-def fit_quadratic_and_output_min_omega(time, omega, t_start=200, t_end=400): # Revisit/Room to investigate and improve. 
+def frequency_cutoff_quad_fit(time, omega, t_start=200, t_end=400): # Revisit/Room to investigate and improve. 
     def quadratic(x, a, b, c):
         return a * x**2 + b * x + c
 
@@ -107,14 +84,10 @@ def fit_quadratic_and_output_min_omega(time, omega, t_start=200, t_end=400): # R
     omega_at_time_zero = np.abs(c)
 
     #print(f"The extremum of the quadratic curve occurs at t = {extremum_x:.15f} with omega = {extremum_y:.15f} . implied omega(t=0) = {omega_at_time_zero:.15f}")
-    #print(f"c = {omega_at_time_zero:.4g}")
-    return 1 #omega_at_time_zero
+    print(f"c = {omega_at_time_zero:.4g}")
+    return omega_at_time_zero
 
-def main():
-    """
-    Main function that reads the gravitational wave data file, processes the data,
-    and saves the output to a file. The input filename is provided via command line.
-    """
+def psi4_fft_to_strain(input_dir="r100", output_dir="new", num_azi_pts=180, colatitude=np.pi/2, s=-2, ell_max=8):
     if len(sys.argv) != 3:
         print("Usage: python3 script.py <path to gw psi dir> <path to output directory>")
         #sys.exit(1)
@@ -124,10 +97,6 @@ def main():
         input_dir = sys.argv[1]
         output_dir = sys.argv[2]
 
-    s = -2
-    ell_max = 8
-    num_azi_pts = 180
-    colatitude = np.pi / 2
     azimuth_values = np.linspace(0, 2 * np.pi, num_azi_pts, endpoint = False)
 
     D = spherical.Wigner(ell_max)
@@ -136,28 +105,27 @@ def main():
     time, mode_data = read_modes_data(input_dir,ell_max)
     strain = np.empty((len(time), num_azi_pts),dtype=object)
     weighted = np.empty((len(time), num_azi_pts),dtype=object)
+    #_, _, phase_dt = cumulative_phase_data(time, [r*i for r,i in mode_data[(2,2)]])
+    omega_0 = 0.05 #frequency_cutoff_quad_fit(time, phase_dt,t_end=400)
 
-    # plt.figure(figsize=(10, 6))
-    # plt.title('psi4 vs Time')
-    # plt.xlabel('Time')
-    # plt.ylabel('sum psi4 weighted')
-    # plt.grid(True)
+    plt.figure(figsize=(10, 6))
+    plt.grid(True)
 
     for idx_azi, azi in enumerate(azimuth_values):
         summation = 0
         for l in range(2, ell_max + 1):
             for m in range(-l, l + 1):
+                # if m == 0 :
+                #     continue
                 real, imag = mode_data[(l, m)]
                 cmplx = real + 1j * imag
                 swsh = sY[idx_azi][D.Yindex(l,m)]
-                summation += cmplx * swsh
+                if l==4 and m==0 :
+                    summation += cmplx * swsh
         weighted[:,idx_azi] = summation
         # Fixed Frequency Integration
         fft_ang_freqs = np.fft.fftfreq(len(time), d = time[1] - time[0]) * 2 * np.pi
         fft_result = np.fft.fft(summation)
-
-        _, _, phase_dt = process_wave_data(time, summation)
-        omega_0 = fit_quadratic_and_output_min_omega(time, phase_dt,t_end=400)
 
         # Just below Eq. 27 in https://arxiv.org/abs/1006.1632
         for idx_omg, omega in enumerate(fft_ang_freqs):
@@ -175,12 +143,11 @@ def main():
                 file.write(f"{t:.16g} {real:.16g} {imag:.16g}\n")
         #print(f"Second time integral data has been saved to {output_file}")
 
-
-
-    #plt.plot(time, [-np.real(x) for x in weighted[:,idx_azi] ])
-        plt.plot(time, [np.real(x) for x in strain[:,idx_azi] ])
-    plt.show()
+    #idx_azi = 0
+    #plt.plot(time, [np.real(x) for x in weighted[:,idx_azi] ])
+    #plt.plot(time, [np.real(x) for x in strain[:,idx_azi] ])
+    #plt.show()
     return strain
 
 if __name__ == "__main__":
-    main()
+    psi4_fft_to_strain()
